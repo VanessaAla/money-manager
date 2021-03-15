5008542f3faccf0c8b0d588381e29ae736f7a88d
YOURGITHUBUSERNAME <YOUR@EMAIL.com>
2021-03-15 15:49:32 +0100

    added a custom amount button using use state while still using redux

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-//import { useState } from "react";
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { deposit, withdraw, reset } from "./store/balance/actions";
 import { selectBalance } from "./store/balance/selectors";
@@ -7,7 +7,7 @@ function App() {
   const dispatch = useDispatch();
   const balance = useSelector(selectBalance);
   //const [balance, setBalance] = useState(0);
-
+  const [customAmount, setCustomAmount] = useState(0); // add custom amount, so we need useState while using Redux
   return (
     <div className="App">
       <p>Balance: {balance}$</p>
@@ -32,6 +32,24 @@ function App() {
         }}
       >
         Reset
+      </button>
+      <br />
+      <br />
+      <input
+        type="number"
+        value={customAmount}
+        onChange={(e) => {
+          setCustomAmount(parseInt(e.target.value));
+        }}
+      />
+      <br />
+      <button
+        onClick={() => {
+          dispatch(deposit(customAmount));
+          setCustomAmount(0);
+        }}
+      >
+        Deposit custom amount
       </button>
     </div>
   );
